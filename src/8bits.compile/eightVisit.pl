3667825471e382c349d2d66050911e5df0b5d2e3
Omar Segura <omarseguvi@gmail.com>
2016-11-09 01:09:37 -0600

    tabla de simbolos para strings

--- src/8bits.compile/eightVisit.pl
+++ src/8bits.compile/eightVisit.pl
@@ -1,20 +1,36 @@
 dynamic simbol/3.
+dynamic simbolS/3.
+/*SimbolS --> Simnbols de String*/
+dynamic cont/1.
 dynamic fun_actual/1.
 
 :-['eightParser']
 .
 
-delete_all:- retractall(fun_actual(_)), retractall(simbol(_,_,_)).
+delete_all:- retractall(fun_actual(_)), retractall(simbol(_,_,_)),retractall(simbolS(_,_,_)).
 
 insert_funActual(N):-  retractall(fun_actual(_)), assert(fun_actual(N)).
 
 insert_simbol(F,V,R):- assert(simbol(F,V,R)).
+
+insert_simbolS(F,V,R):- assert(simbol(F,V,R)).
 
 insert_value(V):- fun_actual(F), atom_concat(F,'_',R),
 								  atom_concat(R,V,R1),
 								  atom_concat(R1,':',R2),
 								  atom_concat(R2,' DB 0;',R3),
 								  insert_simbol(F,V,R3).
+
+insert_string(V):- fun_actual(F), atom_concat(F,'_',R),
+								  atom_concat(R,'string1',R1),
+								  atom_concat(R1,':',R2),
+                  atom_concat(R2,'DB ',R3),
+                  atom_concat(R3,V,R4),
+								  atom_concat(R4,' DB 0;',R5),
+								  insert_simbolS(F,V,R5).
+
+get_id(V,X):- fun_actual(F), simbol(F,V,X).
+get_id(V,X):- fun_actual(F), simbolS(F,V,X).
 
 /*Es solo de prueba */
 
@@ -61,7 +77,7 @@ visit(cll,id(X),Code):- Code = [asmins('CALL', X)]
 visit(assing, id(X), Code) :-  Code = [asmins('POP', 'A'), asmins('MOV', X, 'A')]
 .
 
-visit(id(X), _, Code):- Code = [asmins('PUSH', X)]
+visit(id(X), _, Code):- get_id(X,R), Code = [asmins('PUSH', R)]
 .
 
 visit(num(X), _, Code):- Code = [asmins('PUSH', X)]
