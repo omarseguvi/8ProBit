86ad11b1c9c62164fe08783bf6db7ee9d5ce4482
Omar Segura <omarseguvi@gmail.com>
2016-11-09 21:01:45 -0600

    prints boolean, number y string agregados

--- src/8bits.compile/eightEmiter.pl
+++ src/8bits.compile/eightEmiter.pl
@@ -25,7 +25,7 @@ genCode(Out, eightProg(L)) :- !,
               format(Out,'\tJMP main;\n',[]),
 							format(Out,'\t.UNDEF: DB 255;\n',[]),
 							genCodeList(Out, L),
-              genCodePrint(Out)
+              genCodePrintS(Out)
 .
 /*
 %cambio
@@ -110,7 +110,7 @@ genCodeList(Out, [X, Y | L], Sep) :- genCode(Out, X),
 .
 
 
-genCodePrint(Out) :- !,format(Out,'\n
+genCodePrintS(Out) :- !,format(Out,'\n
 	print_string:
 		POP C
 		POP B
@@ -130,3 +130,53 @@ genCodePrint(Out) :- !,format(Out,'\n
 		RET
                             \n', [])
 .
+
+genCodePrintN(Out) :- !, format(Out,'\n
+print_number:
+	POP C
+	POP A
+	PUSH C
+.number_to_Stack:
+	MOV B,A;
+	DIV 10;
+	MUL 10;
+	SUB B, A;
+	PUSH B;
+	CMP A, 0;
+	JE .number_to_display;
+	DIV 10;
+	JMP .number_to_Stack;
+.number_to_display:
+	POP A;
+	CMP A,C;
+	JE .exit;
+	ADD A, 0x30;
+	MOV [D], A;
+	INC D;
+	JMP .number_to_display;
+	.exit:
+	PUSH .UNDEF
+	PUSH C
+	RET
+  \n',[]).
+
+genCodePrintB(Out) :- !, format(Out,'\n
+print_boolean:
+POP C;
+POP A;
+PUSH C;
+CMP A, 0;
+JNE .print_false;
+PUSH .true
+JMP .pb_exit:
+.print_false:
+PUSH .false
+JMP .pb_exit:
+.pb_exit:
+CALL print_string;
+POP C;
+POP C;
+PUSH .UNDEF
+PUSH C
+RET
+\n',[]).
